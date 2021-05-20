0dc1bfb062ff5b1090598cd302e940b27a339c73
Tomokazu Kozuma <tomokazu.kozuma@gmail.com>
2021-05-20 09:46:20 +0900

    :+1: Update: teller v2.11.0 / postbox v0.9.0

--- gen/gincoinc/adamant/global/v1/adamantglobalv1/model.pb.validate.go
+++ gen/gincoinc/adamant/global/v1/adamantglobalv1/model.pb.validate.go
@@ -1221,6 +1221,16 @@ func (m *Transaction) Validate() error {
 		}
 	}
 
+	if v, ok := interface{}(m.GetStellarSpecific()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return TransactionValidationError{
+				field:  "StellarSpecific",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if v, ok := interface{}(m.GetCreateTime()).(interface{ Validate() error }); ok {
 		if err := v.Validate(); err != nil {
 			return TransactionValidationError{
@@ -2115,6 +2125,77 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = C0BanSpecificValidationError{}
+
+// Validate checks the field values on StellarSpecific with the rules defined
+// in the proto definition for this message. If any rules are violated, an
+// error is returned.
+func (m *StellarSpecific) Validate() error {
+	if m == nil {
+		return nil
+	}
+
+	// no validation rules for SequenceNumber
+
+	// no validation rules for MemoId
+
+	// no validation rules for IsNextSequenceNumber
+
+	return nil
+}
+
+// StellarSpecificValidationError is the validation error returned by
+// StellarSpecific.Validate if the designated constraints aren't met.
+type StellarSpecificValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e StellarSpecificValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e StellarSpecificValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e StellarSpecificValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e StellarSpecificValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e StellarSpecificValidationError) ErrorName() string { return "StellarSpecificValidationError" }
+
+// Error satisfies the builtin error interface
+func (e StellarSpecificValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sStellarSpecific.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = StellarSpecificValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = StellarSpecificValidationError{}
 
 // Validate checks the field values on SignInfo with the rules defined in the
 // proto definition for this message. If any rules are violated, an error is returned.
