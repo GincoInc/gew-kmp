3894f85627227c8b9a9ef998db2f1f12f02cd92c
Karbushev Kirill <KiriosK@yandex.ru>
2025-03-14 15:00:19 +0900

    Add: TON trace id

--- gen/gincoinc/adamant/teller/v1/adamanttellerv1/event.pb.validate.go
+++ gen/gincoinc/adamant/teller/v1/adamanttellerv1/event.pb.validate.go
@@ -192,6 +192,35 @@ func (m *TransferUpdated) validate(all bool) error {
 
 	// no validation rules for Network
 
+	if all {
+		switch v := interface{}(m.GetTonSpecific()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, TransferUpdatedValidationError{
+					field:  "TonSpecific",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, TransferUpdatedValidationError{
+					field:  "TonSpecific",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetTonSpecific()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return TransferUpdatedValidationError{
+				field:  "TonSpecific",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if len(errors) > 0 {
 		return TransferUpdatedMultiError(errors)
 	}
@@ -485,6 +514,110 @@ var _ interface {
 	Cause() error
 	ErrorName() string
 } = TransferUpdateSubstrateSpecificValidationError{}
+
+// Validate checks the field values on TransferUpdateTonSpecific with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *TransferUpdateTonSpecific) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on TransferUpdateTonSpecific with the
+// rules defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// TransferUpdateTonSpecificMultiError, or nil if none found.
+func (m *TransferUpdateTonSpecific) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *TransferUpdateTonSpecific) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for TraceId
+
+	if len(errors) > 0 {
+		return TransferUpdateTonSpecificMultiError(errors)
+	}
+
+	return nil
+}
+
+// TransferUpdateTonSpecificMultiError is an error wrapping multiple validation
+// errors returned by TransferUpdateTonSpecific.ValidateAll() if the
+// designated constraints aren't met.
+type TransferUpdateTonSpecificMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m TransferUpdateTonSpecificMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m TransferUpdateTonSpecificMultiError) AllErrors() []error { return m }
+
+// TransferUpdateTonSpecificValidationError is the validation error returned by
+// TransferUpdateTonSpecific.Validate if the designated constraints aren't met.
+type TransferUpdateTonSpecificValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e TransferUpdateTonSpecificValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e TransferUpdateTonSpecificValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e TransferUpdateTonSpecificValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e TransferUpdateTonSpecificValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e TransferUpdateTonSpecificValidationError) ErrorName() string {
+	return "TransferUpdateTonSpecificValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e TransferUpdateTonSpecificValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sTransferUpdateTonSpecific.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = TransferUpdateTonSpecificValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = TransferUpdateTonSpecificValidationError{}
 
 // Validate checks the field values on TransferUpdateEvent with the rules
 // defined in the proto definition for this message. If any rules are
